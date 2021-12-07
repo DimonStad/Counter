a6d6afb61663c0ff930fa61ac87aaf20dd811f49
DimonStad <57269090+DimonStad@users.noreply.github.com>
2021-12-07 20:15:48 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,26 +24,19 @@ namespace WordCounter
             stopWatch.Start();
             using (var client = new ServiceReference1.Service1Client())
             {
-               
+
                 var ordered = client.GetCounter(str);
                 var sout = new StringBuilder();
                 foreach (KeyValuePair<string, int> kvp in ordered)
                 {
-
                     sout.Append($"{kvp.Key} {kvp.Value}").AppendLine();
-
                 }
-
                 File.WriteAllText(args[1], sout.ToString(), Encoding.UTF8);
-
                 Console.WriteLine(sout);
-
             }
-
 
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
-
 
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
